6e3d60795451ba89ef59858d823601be2f9aab83
Benjamin Tucker <betucker@ucsd.edu>
2017-10-21 16:32:08 -0700

    Add attendee to event and add votes

--- src/main/java/ResponseDb.java
+++ src/main/java/ResponseDb.java
@@ -1,8 +1,4 @@
-import java.security.InvalidParameterException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Calendar;
 import java.util.Objects;
 
@@ -34,11 +30,19 @@ public class ResponseDb {
             // If the responses table doesn't already exist, make it
             connection.createStatement().execute(
                     "create table if not exists Responses ( " +
-                            "Event integer not null, " +
+                            "Event text not null, " +
                             "AttendeePhone text not null, " +
                             "Vote integer not null, " +
                             "Timestamp text not null, " +
-                            "foreign key (Event) references Events (rowid) " +
+                            "foreign key (Event) references Events (Name) " +
+                            ");");
+
+            // If the attendees table doesn't already exist, make it
+            connection.createStatement().execute(
+                    "create table if not exists Attendees ( " +
+                            "AttendeePhone text not null unique, " +
+                            "Event text not null, " +
+                            "foreign key (Event) references Events (Name) " +
                             ");");
 
         } catch (SQLException ex) {
@@ -75,19 +79,66 @@ public class ResponseDb {
     }
 
 
+    public boolean addAttendeeToEvent(String attendeePhone, String eventName) {
+
+        // Validate not null
+        Objects.requireNonNull(attendeePhone);
+        Objects.requireNonNull(eventName);
+
+        // Insert or replace a row
+        try {
+            PreparedStatement stmt = connection.prepareStatement("insert or replace into Attendees (AttendeePhone, Event) values (?, ?, ?);");
+            stmt.setString(1, attendeePhone);
+            stmt.setString(2, eventName);
+            stmt.execute();
+
+        } catch (SQLException ex) {
+            // If the event doesn't exist, return false
+            return false;
+        }
+
+        return true;
+    }
+
+
     public boolean addAttendeeResponse(String phone, AttendeeVote vote, Calendar timestamp) {
 
         // Validate not null
+        Objects.requireNonNull(phone);
+        Objects.requireNonNull(vote);
+        Objects.requireNonNull(timestamp);
 
-        // Find which event this phone number is attending
+        try {
+            // Find which event this phone number is attending
+            PreparedStatement stmt = connection.prepareStatement(
+                    "select (Event) from Attendees " +
+                            "where AttendeePhone = ? ");
+            stmt.setString(1, phone);
+            ResultSet results = stmt.executeQuery();
 
-        // If this phone number is not at an event, return false.
+            // If this phone number is not at an event, return false.
+            if (!results.next()) {
+                // The attendee is not part of an event yet
+                return false;
+            }
 
-        // Insert a row into the responses table
+            String eventName = results.getString(1);
 
-        // Return true on success
+            // Insert a row into the responses table
+            PreparedStatement responseStmt = connection.prepareStatement(
+                    "insert into Responses (Event, AttendeePhone, Vote, Timestamp) values (?,?,?,?);");
+            responseStmt.setString(1, eventName);
+            responseStmt.setString(2, phone);
+            responseStmt.setString(3, vote.name());
+            responseStmt.setString(4, timestamp.toInstant().toString());
+            responseStmt.execute();
 
-        return false; // TODO
+            // Return true on success
+            return true;
+
+        } catch (SQLException ex) {
+            return false;
+        }
     }
 
 
