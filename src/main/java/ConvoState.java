e84c5e7598030da8268a16c0efdc8586a9a3cc94
Catherine Lin <cjl058@ucsd.edu>
2017-10-21 17:16:59 -0700

    Convo enum and switch statements

---
+++ src/main/java/ConvoState.java
@@ -0,0 +1,8 @@
+//list of user cases to keep track of where the user is in the conversation
+public enum ConvoState{
+    UC1_1 , UC1_2, UC1_3
+    UC2_1, UC2_2,
+    UC3_1, UC3_2,
+    UC4,
+    UC5_1, UC5_2, UC5_3, UC5_4;
+}
