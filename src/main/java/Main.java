caa5f81612b10634a2d29b53f634db5043ec130d
Benjamin Tucker <betucker@ucsd.edu>
2017-10-21 19:16:47 -0700

    Extract text body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,11 @@ import com.twilio.twiml.Body;
 import com.twilio.twiml.Message;
 import com.twilio.twiml.MessagingResponse;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static spark.Spark.post;
 
 public class Main {
@@ -23,6 +28,12 @@ public class Main {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         post("/receive-sms", (request, response) -> {
+
+            String body = splitQuery(request.body()).get("Body");
+            if (body == null) {
+                body = "";
+            }
+            body = body.trim().toLowerCase();
 
             // Get previous state
             ConvoState convoState = ConvoState.NONE;
@@ -57,6 +68,23 @@ public class Main {
                 return twiml.toXml();
             }
         });
+    }
+
+
+    private static Map<String, String> splitQuery(String query) {
+
+        try {
+            Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+            String[] pairs = query.split("&");
+            for (String pair : pairs) {
+                int idx = pair.indexOf("=");
+                query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+            }
+            return query_pairs;
+
+        } catch (UnsupportedEncodingException ex) {
+            throw new Error(ex);
+        }
     }
 }
 
